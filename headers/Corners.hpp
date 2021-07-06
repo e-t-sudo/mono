0217c87ec3f6eb252c9736f9c87e85a314570816
e_t <77915530+e-t-sudo@users.noreply.github.com>
2021-07-06 10:30:54 +0530

    Update Corners.hpp

--- headers/Corners.hpp
+++ headers/Corners.hpp
@@ -5,7 +5,7 @@ class Corners{
     public:
         std::string solutionString;
         std::string bufferMove[24] = {"L D F'", "L F2","F","","F D' F2","F D' L' F","","","","L2 F2","L F","L2 D F'","F2","L' F","D F'","D' F2","D' L' F","F'","D2 F2","D2 L' F","D' F'","D F2","L' F2","L2 F"}; //yet to be filled
-        const int buffer = 3;
+        const int buffer = 7;
         typedef struct corner{
             char c1, c2, c3;
         } corner;
